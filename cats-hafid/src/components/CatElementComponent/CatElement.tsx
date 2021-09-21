096f7e3d4c6b5b8d6bf1e2df5ea4ab413a3f3cea
hafidk <hafidstor@gmail.com>
2021-09-21 15:05:09 +0200

    [style] cat element styles

--- cats-hafid/src/components/CatElementComponent/CatElement.tsx
+++ cats-hafid/src/components/CatElementComponent/CatElement.tsx
@@ -4,12 +4,18 @@ import styled from 'styled-components';
 import { usePopoverState, PopoverDisclosure } from "reakit/Popover";
 import { Popover as BasePopover, } from "reakit/Popover";
 
+import {
+  isMobile
+} from "react-device-detect";
 
 const GridItem = styled.div`
   height: 35vh;
   width: 20vw;
+  @media(max-width: 768px) {
+    width: 45vw;
+    height: 40vh;
+  }
   margin: 5px;
-  /* border: 4px solid black; */
   box-shadow: 1px 5px 5px whitesmoke;
   border-radius: 13px;
   background-color: white;
@@ -18,6 +24,10 @@ const GridItem = styled.div`
 const CatImage = styled.div`
   width: 20vw;
   height: 10vw;
+  @media(max-width: 768px) {
+    width: unset;
+    height: 17vh;
+  }
   border-radius: 13px 13px 0px 0px;
   background-repeat:no-repeat;
   background-position:center;
@@ -38,6 +48,9 @@ const PopOverClass = styled.div`
   background-color: #3696e4;
   width: 20vw;
   padding: 10px;
+  @media(max-width: 768px) {
+    width: unset;
+  }
   text-align: justify;
   border-radius: 13px;
 `;
@@ -47,6 +60,7 @@ const ReadMore = styled.div`
   cursor: pointer;
   text-align: left;
   margin: 0px 10px;
+
 `;
 
 function Popover({ disclosure, ...props }) {
@@ -69,13 +83,19 @@ function Popover({ disclosure, ...props }) {
 
 function CatElement(props : any) {
   const getLimitedFact = (fact: string) => {
-    return fact.length >= 100 ? fact.substring(0, 97) + "..." : fact;
+    let len = isMobile ? 70 : 100
+    return fact.length >= len ? fact.substring(0, len - 3) + "..." : fact;
+  }
+
+  const getLimitedLen = () => {
+    let len = isMobile ? 70 : 100
+    return props.fact.length >= len;
   }
   return (
     <GridItem>
       <CatImage style={{ backgroundImage: `url(${"https://cataas.com/cat?timestamp=" + props.uniqueKey})`}}></CatImage>
       <CatDescription>{getLimitedFact(props.fact)}</CatDescription>
-      {props.fact.length >= 100 && <Popover aria-label="Custom popover" disclosure={<ReadMore>Read More</ReadMore>}>
+      {getLimitedLen() && <Popover aria-label="Custom popover" disclosure={<ReadMore>Read More</ReadMore>}>
         <PopOverClass>{props.fact}</PopOverClass>
       </Popover>}
     </GridItem>
