6e380627f99963e45b83122a76072f84971b3cfa
hafidk <hafidstor@gmail.com>
2021-09-21 13:51:52 +0200

    [fix] logo removal

--- cats-hafid/src/App.tsx
+++ cats-hafid/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MainCatsComponent from './components/MainCatsComponent/MainCats';
 import styled from 'styled-components';
